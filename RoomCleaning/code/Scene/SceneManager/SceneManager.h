b793d5cdaba2a5968de083dbeb5fa493ac53f2b3
406-09\student <fca>
2024-11-09 11:50:48 +0900

    SceneManagerの作成

---
+++ RoomCleaning/code/Scene/SceneManager/SceneManager.h
@@ -0,0 +1,53 @@
+#pragma once
+#include<memory>
+#include<stack>
+#include"../../Object/GameObjectManager/GameObjectManager.h"
+
+
+namespace mea
+{
+	/// <summary>
+	/// シーン管理
+	/// </summary>
+	class SceneManager final
+	{
+	public:
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		SceneManager();
+
+		/// <summary>
+		/// デストラクタ
+		/// </summary>
+		~SceneManager();
+
+		/// <summary>
+		/// ゲームループ
+		/// </summary>
+		void GameLoop();
+
+		/// <summary>
+		/// シーン更新処理
+		/// </summary>
+		void UpdateScene();
+
+		/// <summary>
+		/// シーン描画処理
+		/// </summary>
+		void DrawScene();
+
+		/// <summary>
+		/// シーン切り替え処理
+		/// </summary>
+		void ChangeScene();
+
+	private:
+
+		std::shared_ptr<class Fps> mFps;						//タイムマネージャー
+
+		std::stack<std::shared_ptr<class SceneBase>> mNowScene;	//現在のシーン
+		SceneBase* mHoldScene;									//次のシーン
+	};
+
+}
