672d4fe4e0be0905489bbdf6e2a4a2624fe48d18
kono-usagi <kasugano.rins@gmail.com>
2025-01-16 19:20:10 +0900

    Toolsクラスの作成

--- RoomCleaning/code/Object/Cleaning/Inventory/Tools/Tools.cpp
+++ RoomCleaning/code/Object/Cleaning/Inventory/Tools/Tools.cpp
@@ -5,6 +5,38 @@ namespace rc
 {
 	void Tools::MakeItem() 
 	{
-		ToolsArray[] = {};
+		struct InventoryItem ToolsArray[ARRAY_SIZE][ARRAY_SIZE];
+
+		int InventoryID;
+
+		InventoryID = 0;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "タオル");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
+
+		InventoryID = 1;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "洗剤");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
+
+		InventoryID = 2;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "スポンジ");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
+
+		InventoryID = 3;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "布巾");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
+
+		InventoryID = 4;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "掃除機");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
+		
+		InventoryID = 5;
+		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "ゴミ箱");
+		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/simple_hotel_room02.png");
+		ToolsArray[InventoryID][2].ItemID = 0;
 	}
 }
