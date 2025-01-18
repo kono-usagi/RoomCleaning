88877666f4c6f634d80c131f180c863bbbf8665b
406-09\student <fca>
2025-01-18 12:17:22 +0900

    画像の追加

--- RoomCleaning/code/Object/Cleaning/Inventory/Amenities/Amenities.cpp
+++ RoomCleaning/code/Object/Cleaning/Inventory/Amenities/Amenities.cpp
@@ -5,38 +5,38 @@ namespace rc
 {
 	void Amenities::MakeItem()
 	{
-		struct InventoryItem ToolsArray[ARRAY_SIZE][ARRAY_SIZE];
+		struct InventoryItem AmenitiesArray[ARRAY_SIZE][ARRAY_SIZE];
 
 		int InventoryID;
 
 		InventoryID = 0;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "タオル");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/souji_zoukin.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "歯ブラシ");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/TOOTHBRUSH.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;
 
 		InventoryID = 1;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "洗剤");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/senzai_syokki.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "ボディタオル");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/senzai_syokki.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;
 
 		InventoryID = 2;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "スポンジ");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/cooking_sponge.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "ティッシュ");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/tissue_paper_box_open.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;
 
 		InventoryID = 3;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "布巾");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/nuno.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "トイレットペーパー");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/nuno.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;
 
 		InventoryID = 4;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "掃除機");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/oosouji_soujiki.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "水");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/bottle_water.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;
 
-		InventoryID = 5;
-		strcpy_s(ToolsArray[InventoryID][0].ItemDisplayName, 10, "ゴミ箱");
-		ToolsArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/gomibako_full.png");
-		ToolsArray[InventoryID][2].ItemID = 0;
+		/*InventoryID = 5;
+		strcpy_s(AmenitiesArray[InventoryID][0].ItemDisplayName, 10, "スリッパ");
+		AmenitiesArray[InventoryID][1].ItemImageHandle = LoadGraph("../asset/Image/.png");
+		AmenitiesArray[InventoryID][2].ItemID = 0;*/
 	}
 }
