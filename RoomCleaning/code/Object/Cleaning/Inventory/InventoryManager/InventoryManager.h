71ec70bbbc15a21d7e3b910f1236bf17ff68c432
406-08\student <fca>
2025-01-25 12:20:23 +0900

    ボタンの実装準備

--- RoomCleaning/code/Object/Cleaning/Inventory/InventoryManager/InventoryManager.h
+++ RoomCleaning/code/Object/Cleaning/Inventory/InventoryManager/InventoryManager.h
@@ -1,12 +1,17 @@
 #pragma once
 
 //#include"../InventoryViewModel/InventoryViewModel.h"
+#include"../../RoomCleaning/code/Object/Input/Input.h"
+#include"../../RoomCleaning/code/Button/OnClickListener.h"
+#include"../../RoomCleaning/code/Button/Button.h"
 #include"../InventoryView/InventoryView.h"
 #include"../../Inventory/Tools/Tools.h"
 #include"../../Inventory/Amenities/Amenities.h"
 #include"../../Inventory/Linens/Linens.h"
-#include"../../RoomCleaning/code/Object/Input/Input.h"
 
+#include"DxLib.h"
+
+class Button;
 
 namespace rc
 {
@@ -14,23 +19,38 @@ namespace rc
     {
     public:
         InventoryManager();//コンストラクタ
+        ~InventoryManager();
 
         void Update();
         
-        
-
         //現在のタブ変更
         void ChangeTab(int tab);
+
+        void MouseInput();
+        void Draw();
+
 
         //表示するページ　**デバッグ用
         //DrawFormatString(0, 0, GetColor(255, 255, 255), mNowTab);
 
     private:
-
         int mNowTab=0;//現在のタブ
 
         InventoryView inventoryView;
         Input input;
+
+        //ボタン
+        Button* m_rect_bt;
+        int m_count;
+        class CountUpListener :public OnClickListener
+        {
+        public:
+            CountUpListener(InventoryManager& obj):m_parent(obj){}
+            void OnClick() override{m_parent.m_count++;}
+
+        private:
+            InventoryManager& m_parent;
+        };
 
     };
 }
